e7a6195349125a4c8564726e9c467cd293a5ed34
Pedro Alejandro Rodríguez Orellana <pedroaro123@gmail.com>
2018-01-06 17:17:03 -0400

    Cambiar index por inicio, personalizacion y ajustes menores

--- js/perfil.js
+++ js/perfil.js
@@ -45,3 +45,14 @@ function LoadPhotos(){
 function ModoNocturno() {
   document.body.classList.toggle('nightmode');
 };
+
+$(document).ready(function(){
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  date_input.datepicker({
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  })
+})
